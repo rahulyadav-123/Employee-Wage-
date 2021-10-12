db123712ce3aaa3f28b6354d492857a319242b3a
rahul yadav <ry219613@gmail.com>
2021-10-12 21:45:09 +0530

    calculate salary of employee

--- com/Empwage/empWage.java
+++ com/Empwage/empWage.java
@@ -5,43 +5,28 @@ public class empWage {
 		int empHour;
 		final int full_time = 1;
 		final int part_time = 2;
-		int total_salary = 0;
-		int working_days = 20;
 		int empRateprHr = 20;
-		int f = 0;
-		int p = 0;
-		int s = 0;
-		for(int i=0; i<=working_days; i++){
-			
-			int empcheck = (int) Math.floor(Math.random()*3);
-
-			switch(empcheck) {
-				case full_time:
-					empHour = 8;
-					f++;
-					break;
-				case part_time:
-					empHour = 4;
-					p++;
-					break;
-				default:
-					empHour = 0;
-					s++;
-			}
-			
-			int salary = empRateprHr*empHour;
-			
-			total_salary += salary;
-		}
-	
-		System.out.println(f + " " + p + " " + s);
-		System.out.println("Monthly salary of employee is " + total_salary);
 		
+		int empcheck = (int) Math.floor(Math.random()*3);
+
+		if(empcheck == full_time) {
+			empHour = 10;
+		}
+		else if(empcheck == part_time){
+			empHour = 8;
+		}
+		else{
+			empHour = 0;
+		}
+		
+		int salary = empRateprHr*empHour;
+		System.out.println("The salary of employee is " + salary);
 	}
+
+
+}
+
 
 	
 
-
-
-	}
-
+	
