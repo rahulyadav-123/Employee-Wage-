0da31420e31e270b18f7e803203b94198a43e25e
rahul yadav <ry219613@gmail.com>
2021-10-12 19:01:44 +0530

    Calculate employee present or absent

--- com/Empwage/empWage.java
+++ com/Empwage/empWage.java
@@ -2,8 +2,24 @@ package com.Empwage;
 
 public class empWage {
 	public static void main(String[] args) {
-		System.out.println("welcome Emp wage problem");
-	
-}
+		
+			  // Constants
+	        final int IS_FULL_TIME = 1;
+	        final int EMP_RATE_PER_HOUR = 20;
 
+	        // Initializing variables
+	        int EmpWage = 0, empHrs = 0;
+
+	        // Computations
+	        double empCheck = Math.floor(Math.random() * 10) % 2;
+
+	          if(empCheck == IS_FULL_TIME) {
+	               System.out.println("Employee is Present");
+	                 empHrs = 8;
+	                  }
+	               else {
+	                System.out.println("Employee is Absent");
+		}
+
+		}
 }
