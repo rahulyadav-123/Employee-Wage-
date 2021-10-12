5563c4263a3e13221c466da37c3661a391daf126
rahul yadav <ry219613@gmail.com>
2021-10-12 21:28:53 +0530

    calculate daily employee wage

--- com/Empwage/empWage.java
+++ com/Empwage/empWage.java
@@ -2,31 +2,30 @@ package com.Empwage;
 
 public class empWage {
 	public static void main(String[] args) {
-		  // Constants
+		
+	// Constants
 
-        final int IS_PART_TIME = 1;
-        final int IS_FULL_TIME = 2;
-        final int EMP_RATE_PER_HOUR = 20;
+    final int IS_PART_TIME = 1;
+    final int IS_FULL_TIME = 2;
+    final int EMP_RATE_PER_HOUR = 20;
 
-        // Initializing variables
-        int EmpWage = 0, empHrs = 0;
+    	// Initializing variables
+    	int EmpWage = 0, empHrs = 0;
 
-        // Computations
-        double empCheck = Math.floor(Math.random() * 10) % 3;
+    	// Computations
+    	double empCheck = Math.floor(Math.random() * 10) % 3;
 
-          if(empCheck == IS_FULL_TIME) {
-                    empHrs = 8;
-                 }
-               else if(empCheck == IS_PART_TIME) {
-                    empHrs = 4;
-                 }
-               else {
-                    empHrs = 0;
-                 }
-                  EmpWage = empHrs * EMP_RATE_PER_HOUR;
-           System.out.println("To Calculate Dailly Employee Wage: "+EmpWage);
-      }
-
-	}
-	
+    	if(empCheck == IS_FULL_TIME) {
+                empHrs = 8;
+             }
+           else if(empCheck == IS_PART_TIME) {
+                empHrs = 4;
+             }
+           else {
+                empHrs = 0;
+             }
+              EmpWage = empHrs * EMP_RATE_PER_HOUR;
+       System.out.println("To Calculate Dailly Employee Wage: "+EmpWage);
+  }
+}
 
